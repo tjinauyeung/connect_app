fcedd17655b2d95fec73f5ae5517b15c798e93d9
Tjin Au Yeung <tjinauyeung@gmail.com>
2016-03-15 17:54:27 +0100

    added more input options

--- src/About.js
+++ src/About.js
@@ -1,108 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-
 class About extends React.Component {
-	constructor(){
-		super();
-		this.state = {
-			toggleAbout: true
-		}
+  constructor(){
+	super();
+	this.state = {
+	  toggleAbout: true
 	}
+  }
 
-	toggleAboutPage() {
-		this.setState({
-			toggleAbout: !this.state.toggleAbout
-		})
+  toggleAboutPage() {
+	this.setState({
+	  toggleAbout: !this.state.toggleAbout
+	})
+  }
+
+  render() {
+
+
+	//show about button
+	if (this.state.toggleAbout == true) {
+	  return (
+		  <div className="about--closebutton">
+			  <h2 onClick={this.toggleAboutPage.bind(this)}>About</h2>
+		  </div>
+	  );
+
+	//show about page 
+	} else {
+	  return (
+		<div className="about--wrapper">
+		  <div className="about--content">
+			<h1>Our mission</h1>
+			<h2 className="about--description">
+			  We're Follow. We're here to connect people through their favorite restaurants.
+			  <br/>Because good food brings good people. &#9829;
+			</h2>
+		  </div>
+		  <h2 onClick={this.toggleAboutPage.bind(this)} className="about--closebutton">Close</h2>
+		  <img className="logo" src="http://www.tjinauyeung.nl/logolight.png" />
+		  <footer className="about--footer">
+			<h4>A React.js demo by Matt la Porte & <a href="http://www.tjinauyeung.nl" target="_blank"><span>Tjin Au Yeung</span></a> &mdash; for more info visit <a href="http://www.https://github.com/tjinauyeung/connect_app"><span>Git repo</span></a></h4>
+		  </footer>
+		</div>
+	  );
 	}
-
-	render() {
-
-		//start styling
-		let about = {
-			position: 'fixed',
-			right: '40px',
-			top: '20px',
-			zIndex: '2'
-		}
-		let wrapper = {
-			position: 'fixed',
-			top: '0',
-			left: '0',
-			right: '0',
-			bottom: '0',
-			zIndex: '4',
-			background: '-webkit-linear-gradient(top, #e06471 0%,#ff7c7c 100%)',
-			display: 'flex',
-			height: '100vh',
-			width: '100vw',
-			justifyContent: 'center',
-			alignItems: 'center'
-		}
-		let close = {
-			position: 'fixed',
-			top: '20px',
-			right: '40px',
-			zIndex: '3',
-			color: 'white'
-		}
-		let content = {
-			textAlign: 'center',
-			color: 'white',
-			padding: '20%'
-		}
-		let logo = {
-      position: 'fixed',
-      top: '30px',
-      left: '0px',
-      right: '0px',
-      width: '450px',
-      margin: 'auto',
-      zIndex: '4'
-    }
-    let mission = {
-    	lineHeight: '35px',
-    	fontWeight: 'normal'
-    }
-    let footer = {
-    	zIndex: '12',
-    	position: 'fixed',
-    	bottom: '20px',
-    	left: '40px',
-    	color: '#fff'
-    }
-		//end styling
-
-
-		//show about button
-		if (this.state.toggleAbout == true) {
-			return (
-					<div style={about}>
-							<h2 onClick={this.toggleAboutPage.bind(this)}>About</h2>
-					</div>
-			);
-
-		//show about page	
-		} else {
-			return (
-				<div style={wrapper}>
-					<div style={content}>
-						<h1>Our mission</h1>
-						<h2 style={mission}>
-							We're Follow. We're here to connect people through their favorite restaurants.
-							<br/>Because good food brings good people. &#9829;
-						</h2>
-					</div>
-					<h2 onClick={this.toggleAboutPage.bind(this)} style={close}>Close</h2>
-					<img style={logo} src="http://www.tjinauyeung.nl/logolight.png" />
-					<footer style={footer}>
-            <h4>A React.js demo by Matt & <a href="http://www.tjinauyeung.nl" target="_blank">Tjin</a>
-             - for more info visit <a href="http://www.https://github.com/tjinauyeung/connect_app">Github</a></h4>
-          </footer>
-				</div>
-			);
-		}
-	}
+  }
 }
 
 export default About;
--- src/Model.js
+++ src/Model.js
@@ -15,17 +15,34 @@ class Model {
 			let korean = request.search(/korean/ig);
 			let kimchi = request.search(/kimchi/ig);
 			let steak = request.search(/steak/ig);
+			let grill = request.search(/gril/ig);
+			let french = request.search(/french/ig);
+			let escargots = request.search(/escargots/ig);
+			let foigras = request.search(/foi gras/ig);
 			let spaghetti = request.search(/spaghetti/ig);
-			let surprise = request.search(/surprise/ig);
+			let pizza = request.search(/pizza/ig);
+			let italian = request.search(/italian/ig);
+			let chinese = request.search(/chinese/ig);
+			let dimsum = request.search(/dimsum/ig);
+			let wok = request.search(/wok/ig);
+			let sushi = request.search(/sushi/ig);
+			let ramen = request.search(/ramen/ig);
+			let japanese = request.search(/japanese/ig);
 
 			if (thai >= 0 || padthai >= 0 ) {
 				sendRequest("thai", onDone);
 			} else if (korean >= 0 || kimchi >= 0) {
 				sendRequest("korean", onDone);
-			}	else if (steak >= 0) {
+			} else if (steak >= 0 || grill >= 0) {
 				sendRequest("steak", onDone);
-			} else if (surprise >= 0 ) {
-				sendRequest("surprise", onDone);
+			}	else if (escargots >= 0 || french >= 0 || foigras >= 0) {
+				sendRequest("french", onDone);
+			} else if (spaghetti >= 0 || pizza >= 0 || italian >= 0 ) {
+				sendRequest("italian", onDone);
+			} else if (chinese >= 0 || dimsum >= 0 || wok >= 0 ) {
+				sendRequest("chinese", onDone);
+			} else if (japanese >= 0 || sushi >= 0 || ramen >= 0 ) {
+				sendRequest("japanese", onDone);
 			} else {
 				jQuery('#requestnotfound').html(
 					"Can't help you with that.. Try something else!");
